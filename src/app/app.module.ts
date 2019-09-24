90eeff45c075e0edc1df5c499a23d7a137c85537
mrbigwolf67 <uffec@comhem.se>
2019-09-24 15:06:05 +0200

    Added home and websites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './app-layout/app-layout/app-layout.component';
+import { SiteLayoutComponent } from './app-layout/site-layout/site-layout.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing/routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShoppingComponent } from './websites/shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    SiteLayoutComponent,
+    HomeComponent,
+    NavbarComponent,
+    ShoppingComponent
   ],
   imports: [
+    RoutingModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:   [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { SiteLayoutComponent } from '../app-layout/site-layout/site-layout.component';
+import { HomeComponent } from '../home/home.component';
+import { AppLayoutComponent } from '../app-layout/app-layout/app-layout.component';
+import { ShoppingComponent } from '../websites/shopping/shopping.component';
+
+const routes: Routes = [
+  // Site routes goes here
+  { path: '',  component: SiteLayoutComponent ,
+     children:  [
+       { path: '', component: HomeComponent, pathMatch: 'full'}
+     ] },
+      // App routes goes here here
+     {
+       path: '', component: AppLayoutComponent,
+        children: [
+          {
+            path: 'shopping', component: ShoppingComponent
+          },
+          // {
+          //   path: 'coming-soon', component: ComingSoonComponent
+          // },
+          // {
+          //   path: 'dashboard', component: DashboardComponent
+          // },
+          // {
+          //   path: 'webgrid', component: WebgridComponent
+          // },
+          // {
+          //   path: 'adventure', component: AdventureComponent
+          // },
+          // {
+          //   path: 'sticky', component: StickyComponent
+          // },
+          // {
+          //   path: 'login', component: LoginComponent
+          // },
+          // {
+          //   path: 'store', component: ShoppingCartComponent
+          // }
+        ]
+     },
+
+     // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class RoutingModule {}
